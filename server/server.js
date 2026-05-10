3f1a9c029a82e39a08eebddad70dc95db0105a1c
itzshen <shensmartboyz@gmail.com>
2026-05-10 22:47:38 +0700

    add for host

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const weatherRoutes = require('./routes/weatherRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
